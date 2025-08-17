e515b84ff8c2a52f555fe3da96f0fdc755e425d3
rileyyyyyyyyyy <167968952+rileyyyyyyyyyy@users.noreply.github.com>
2025-08-18 08:16:06 +1000

    Moves current logic into a class

--- main.py
+++ main.py
@@ -9,53 +9,67 @@ TENANT_ID = os.getenv('TENANT_ID')
 APP_ID = os.getenv('APP_ID')
 APP_SECRET = os.getenv('APP_SECRET')
 
-url = 'https://login.microsoftonline.com/%s/oauth2/token' % (TENANT_ID)
 
-RESOURCE_APP_ID_URI = 'https://api.securitycenter.microsoft.com'
-
-body = {
-    'resource': RESOURCE_APP_ID_URI,
-    'client_id': APP_ID,
-    'client_secret': APP_SECRET,
-    'grant_type': 'client_credentials'
-}
-
-with requests.Session() as s:
-    r = s.get(url=url, data=body)
-    data = r.json()
+class Requestor:
+    def __init__(self, tenant_id, app_id, app_secret):
+        self._token_url = 'https://login.microsoftonline.com/%s/oauth2/token' % (tenant_id)
+        self._resource_app_id_uri = 'https://api.securitycenter.microsoft.com'
+        self._body = {
+            'resource': RESOURCE_APP_ID_URI,
+            'client_id': APP_ID,
+            'client_secret': APP_SECRET,
+            'grant_type': 'client_credentials'
+        }
+        self._aad_token = None
+        
+    def open_session(self):
+        return requests.Session()
     
-    AAD_TOKEN = data['access_token']
+    def _get_token(self, session):
+        request = session.get(url=url, data=body)
+        data = request.json()
+        
+        self._aad_token = data['access_token']
 
-    s.headers.update({
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': f'Bearer {AAD_TOKEN}'
-    })
+        session.headers.update({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {self._aad_token}'
+        })
 
-    all_rows = []
-    url = 'https://api.securitycenter.microsoft.com/api/vulnerabilities'
+    def request_data(self, session):
+        if self.__aad_token is None:
+            self._get_token(session)
+        
+        all_rows = []
+        url = 'https://api.securitycenter.microsoft.com/api/vulnerabilities'
+        
+        while url:
+            response = session.get(url, timeout=120)
+            response.raise_for_status()
+            data = response.json()
+            rows = data.get('value', [])
+            all_rows.extend(rows)
+
+            next_link = data.get('@odata.nextLink')
+            if next_link:
+                url = next_link
+            else:
+                url = None
+
+        fieldnames = all_rows[0].keys()
+        
+        with open('output.csv', 'w', newline='', encoding='utf-8-sig') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames, extrasaction='ignore')
+            writer.writeheader()
+            writer.writerows(all_rows)
+
+        print(f'{len(all_rows)} rows written to output.csv')
+
+
+
+requestor = Requestor(TENANT_ID, APP_ID, APP_SECRET)
+
+with requestor.open_session() as s:
+    requestor.request_data(s)
     
-    while url:
-        r = s.get(url, timeout=120)
-        r.raise_for_status()
-        data = r.json()
-        rows = data.get('value', [])
-        all_rows.extend(rows)
-
-        next_link = data.get('@odata.nextLink')
-        if next_link:
-            url = next_link
-        else:
-            url = None
-
-    fieldnames = all_rows[0].keys()
-    
-    with open('vulnerabilities.csv', 'w', newline='', encoding='utf-8-sig') as f:
-        writer = csv.DictWriter(f, fieldnames=fieldnames, extrasaction='ignore')
-        writer.writeheader()
-        writer.writerows(all_rows)
-
-    print(f'{len(all_rows)} rows written to vulnerabilities.csv')
-
-
-
